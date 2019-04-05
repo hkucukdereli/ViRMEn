2542c9ae025cf8e3c1be9f1e7640bbe4a22ee20c
Hakan Kucukdereli <hkucukdereli@gmail.com>
2019-04-04 22:28:36 -0400

    Spacebar for trial init. Serial functionality.

--- experiments/Conditioning.m
+++ experiments/Conditioning.m
@@ -19,10 +19,11 @@ function vr = initializationCodeFun(vr)
                         'date', '190401',...
                         'run', 1,...
                         'rig', 'VR_training',...
+                        'serial', false,...
                         'startTime', now(),...
-                        'trialDuration', 5,...
+                        'trialDuration', 15,...
                         'blackOut', false,...
-                        'inTrial', true,...
+                        'inTrial', false,...
                         'blackOutDuration', 2);
                     
     vr.sessionData = struct('position',[],...
@@ -34,37 +35,48 @@ function vr = initializationCodeFun(vr)
     vr.trialInfo(1:1000) = struct('trialNum', 0,...
                                   'trialType', 0,...
                                   'stimOn', 0);
-    
+    if vr.session.serial
+        vr = initializationForSerial(vr, 4);
+    end
+                              
     vr.trialDuration = [vr.session.trialDuration];
     vr.trialNumber = 1;
     vr.lastPos = 0;
-    tic;
-    vr.trialStart = vr.timeElapsed;
+    
+    vr.worlds{vr.currentWorld}.surface.visible(1,:) = 0;
+    
+    vr.previousTime = vr.timeElapsed;
     
 % --- RUNTIME code: executes on every iteration of the ViRMEn engine.
 function vr = runtimeCodeFun(vr)
-    
-    if ~vr.session.blackOut && vr.session.inTrial && vr.timeElapsed - vr.trialStart > vr.trialDuration
-        vr.currentWorld
-        vr.timeElapsed
+    % Press space to start
+    if vr.keyPressed == 32
+        vr.worlds{vr.currentWorld}.surface.visible(1,:) = 1;
+        vr.session.inTrial = true;
+        vr.previousTime = vr.timeElapsed;
+    end
+
+    if vr.session.blackOut == false & vr.session.inTrial == true & (vr.timeElapsed - vr.previousTime) > vr.trialDuration
         % change the world to the next one and teleport to the start
         vr.worlds{vr.currentWorld}.surface.visible(1,:) = 0;
         % change states
         vr.session.blackOut = true;
         vr.session.inTrial = false;
         % timestamp
-        vr.trialEnd = vr.timeElapsed;
+        vr.previousTime = vr.timeElapsed;
+    end
+    
+    if vr.session.blackOut == true & vr.session.inTrial ==false & (vr.timeElapsed - vr.previousTime) > vr.session.blackOutDuration
+        % make the world visible
+        vr.worlds{vr.currentWorld}.surface.visible(1,:) = 1;
+        % change states
+        vr.session.blackOut = false;
+        vr.session.inTrial = true;
+        % timestamp
+        vr.previousTime = vr.timeElapsed;
         
         % set the next trial duration
         vr.trialDuration = [vr.trialDuration, normrnd(vr.session.trialDuration, 1, 1)];
-    end
-    
-    if vr.session.blackOut && ~vr.session.inTrial && vr.timeElapsed - vr.trialEnd > vr.session.blackOutDuration
-        vr.currentWorld
-        vr.timeElapsed - vr.trialEnd
-        vr.worlds{vr.currentWorld}.surface.visible(1,:) = 1;
-        vr.session.blackOut = false;
-        vr.trialStart = vr.timeElapsed;
         
         % change the world to the next one and teleport to the start
         vr.currentWorld = mod(vr.currentWorld, length(vr.exper.worlds)) + 1;
@@ -72,27 +84,19 @@ function vr = runtimeCodeFun(vr)
         vr.position(2) = initPos(2); % set the animal’s y position to 0
         vr.dp(:) = 0; % prevent any additional movement during teleportation
         
+        % turn on/off the stim
+        if vr.session.serial
+            if any(strcmp(fieldnames(vr.worlds{vr.currentWorld}.objects.indices), 'CueStripe135'))
+                arduinoWriteMsg(vr.arduino_serial, 'S');
+            elseif any(strcmp(fieldnames(vr.worlds{vr.currentWorld}.objects.indices), 'CueStripe135'))
+                arduinoWriteMsg(vr.arduino_serial, 'O');
+            end
+        end
+        
         vr.trialNumber = vr.trialNumber + 1;
         vr.sessionData.trialNumber = [vr.sessionData.trialNumber, vr.trialNumber];
         vr.lastPos = 0;
     end
-    
-%     if vr.session.blackOut == 0 && vr.timeElapsed - vr.trialStart > vr.trialDuration
-%         % change the world to the next one and teleport to the start
-%         vr.currentWorld = mod(vr.currentWorld, length(vr.exper.worlds)) + 1;
-%         initPos = vr.worlds{vr.currentWorld}.startLocation;
-%         vr.position(2) = initPos(2); % set the animal’s y position to 0
-%         vr.dp(:) = 0; % prevent any additional movement during teleportation
-%         vr.trialDuration = [vr.trialDuration, normrnd(vr.session.trialDuration, 1, 1)];
-%         
-%         vr.trialNumber = vr.trialNumber + 1;
-%         vr.sessionData.trialNumber = [vr.sessionData.trialNumber, vr.trialNumber];
-%         vr.lastPos = 0;
-%         
-%         vr.worlds{vr.currentWorld}.surface.visible(1,:) = 0;
-%         vr.session.blackOut = 1;
-%         vr.trialEnd = vr.timeElapsed;
-%     end
 
     vr = teleportCheck(vr);
     
