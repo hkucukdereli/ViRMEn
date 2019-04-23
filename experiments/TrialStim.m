2c995f095ae13ace1c3d8788e236e1e7d81fe806
Hakan Kucukdereli <hkucukdereli@gmail.com>
2019-04-23 16:26:33 -0400

    Trial code is added.

--- experiments/TrialStim.m
+++ experiments/TrialStim.m
@@ -20,11 +20,13 @@ function vr = initializationCodeFun(vr)
                         'date', '190405',...
                         'run', 1,...
                         'rig', 'VR_training',...
+                        'experiment', 'testing',...
+                        'basedir', 'C:/Users/hkucukde/Dropbox/Hakan/AndermannLab/code/MATLAB/ViRMEn',...
                         'timeout', 60,...
                         'blackOutDuration', 2,...
                         'cueList', struct('stim', 'CueStripe45',...
                                           'neutral','CueStripe135'),...
-                        'serial', false,...
+                        'serial', true,...
                         'com', 5,...
                         'inTrial', false,...
                         'timeOut', true,...
@@ -47,7 +49,6 @@ function vr = initializationCodeFun(vr)
         vr = initializationForSerial(vr, vr.session.com);
     end
                               
-%     vr.trialDuration = [vr.session.trialDuration];
     vr.nTrials = 0;
     vr.lastPos = 0;
     
@@ -77,7 +78,18 @@ function vr = runtimeCodeFun(vr)
         % initialize the trial count
         vr.sessionData.nTrials = 1;
         vr.trialInfo(vr.sessionData.nTrials).trialNum = vr.sessionData.nTrials;
-%         vr.trialInfo(vr.sessionData.nTrials).trialDuration = vr.session.trialDuration;
+        
+        if vr.currentCue == vr.session.cueList.('stim')
+            vr.onStim = true;
+            if vr.session.serial
+                arduinoWriteMsg(vr.arduino_serial, 'S');
+            end
+        elseif vr.currentCue == vr.session.cueList.('neutral')
+            vr.onStim = false;
+            if vr.session.serial
+                arduinoWriteMsg(vr.arduino_serial, 'O');
+            end
+        end
     elseif vr.waitOn & ~(vr.keyPressed == 32)
         vr.position(2) = vr.worlds{vr.currentWorld}.startLocation(2);
     end
@@ -92,27 +104,29 @@ function vr = runtimeCodeFun(vr)
             vr.currentCue = vr.cuelist(p);
         end
     end
-    
-    num2str(vr.position(2))
 
     if vr.position(2) > vr.positions(end-3)
         vr.position(2) = vr.initPos(2);
         vr.dp(:) = 0;
-        vr.currentWorld = round(vr.currentWorld) + 1
+        vr.currentWorld = round(vr.currentWorld) + 1;
     end
 
-    
     % only do something if the cue has changed
-%     if strcmp(vr.previousCue, vr.currentCue)
-%         if vr.currentCue == vr.session.cueList.('stim')
-%             vr.onStim = true;
-%         elseif vr.currentCue == vr.session.cueList.('neutral')
-%             vr.onStim = false;
-%         end
-%         % update the previous cue because the cue has changed
-%         vr.previousCue = vr.currentCue;
-%     end
-    
+    if ~strcmp(vr.previousCue, vr.currentCue)
+        if vr.currentCue == vr.session.cueList.('stim')
+            vr.onStim = true;
+            if vr.session.serial
+                arduinoWriteMsg(vr.arduino_serial, 'S');
+            end
+        elseif vr.currentCue == vr.session.cueList.('neutral')
+            vr.onStim = false;
+            if vr.session.serial
+                arduinoWriteMsg(vr.arduino_serial, 'O');
+            end
+        end
+        % update the previous cue because the cue has changed
+        vr.previousCue = vr.currentCue;
+    end
     
     vr.sessionData.position = [vr.sessionData.position, vr.position(2) + vr.lastPos];
     vr.sessionData.velocity = [vr.sessionData.velocity, vr.velocity(2)];
@@ -128,7 +142,7 @@ function vr = terminationCodeFun(vr)
 
     sessionData = vr.sessionData;
     trialInfo = vr.trialInfo;
-    save(sprintf('data/%s_%s_%i.mat', vr.session.mouse, vr.session.date, vr.session.run),...
+    save(sprintf('%s/data/%s_%s_%i_%s.mat', vr.session.basedir, vr.session.mouse, vr.session.date, vr.session.run, vr.session.experiment),...
         'sessionData', 'trialInfo');
     if vr.session.serial
         terminationForSerial(vr);
