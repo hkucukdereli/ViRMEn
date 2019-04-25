f6e36b7e1dac1a171d4f701b8e654bc61a039cb6
Trent Pottala <trentpottala@gmail.com>
2019-04-25 14:02:57 -0400

    Pulsing fixed.

--- functions/terminationForSerial.m
+++ functions/terminationForSerial.m
@@ -1,10 +1,6 @@
 function terminationForSerial(vr)
     % Close the log file
 %     fclose(vr.fid);
-    
-    % Turn off stim if on
-    if vr.nTrials
-        arduinoWriteMsg(vr.arduino_serial, 'O');
-    end
+   
     % Close the open serial port
     arduinoClose(vr.arduino_serial);
