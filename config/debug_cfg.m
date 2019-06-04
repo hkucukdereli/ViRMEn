6e72169a4575ff04af9b478d7c9a4dd4c477f83f
Kucukdereli <hkucukde@its.caregroup.org>
2019-06-03 22:35:17 -0400

    Bug fixes

--- config/debug_cfg.m
+++ config/debug_cfg.m
@@ -8,16 +8,16 @@ vrconfig.serial = true;
 vrconfig.com = 13;
 
 % Habituation
-vrconfig.habituationDuration = .2; % 
+vrconfig.habituationDuration = .2; % min
 
 % Conditioning
 vrconfig.conditioningDuration = .2; % min
 vrconfig.blackoutDuration = .2; % min
 
 % Trial
-vrconfig.trialDuration = .1; % min
+vrconfig.trialDuration = 5; % min
 vrconfig.timeoutDuration = .2; % sec
-vrconfig.paddingDuration = .2   ; % sec
+vrconfig.paddingDuration = .2   ; % min
 
 % Stress
 vrconfig.stressDuration = .2; % min
