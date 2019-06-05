3d58025e4a32c8368d61cc1d764c41f4b4591f0e
Hakan Kucukdereli <hkucukdereli@gmail.com>
2019-06-05 15:00:10 -0400

    Cue type data type fixed

--- functions/stimOff.m
+++ functions/stimOff.m
@@ -2,6 +2,6 @@ function vr = stimOff(vr)
     if vr.session.serial
         vr.sessionData.stimoff = [vr.sessionData.stimoff, [vr.timeElapsed, vr.position(2) + vr.lastPos]];
         vr.sessionData.cueid = [vr.sessionData.cueid, vr.cueid];
-        vr.sessionData.cuetype = [vr.sessionData.cuetype, vr.currentCue];
+        vr.sessionData.cuetype = [vr.sessionData.cuetype, {vr.currentCue}];
         arduinoWriteMsg(vr.arduino_serial, 'O');
     end
--- functions/stimOn.m
+++ functions/stimOn.m
@@ -2,6 +2,6 @@ function vr = stimOn(vr)
     if vr.session.serial
         vr.sessionData.stimon = [vr.sessionData.stimon, [vr.timeElapsed, vr.position(2) + vr.lastPos]];
         vr.sessionData.cueid = [vr.sessionData.cueid, vr.cueid];
-        vr.sessionData.cuetype = [vr.sessionData.cuetype, vr.currentCue];
+        vr.sessionData.cuetype = [vr.sessionData.cuetype, {vr.currentCue}];
         arduinoWriteMsg(vr.arduino_serial, 'S');
     end
