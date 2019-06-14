b292561014d9df0c254f5384010a7e7145587bd6
Hakan Kucukdereli <hkucukdereli@gmail.com>
2019-06-14 14:20:44 -0400

    Commit again.

--- config/debug_cfg.m
+++ config/debug_cfg.m
@@ -4,11 +4,7 @@ vrconfig.rig = 'vr_rig';
 vrconfig.basedir = 'C:/Users/hkucukde/Dropbox/Hakan/AndermannLab/code/MATLAB/ViRMEn';
 
 % Serial communication
-<<<<<<< HEAD
 vrconfig.serial = true;
-=======
-vrconfig.serial = false;
->>>>>>> refs/remotes/origin/master
 vrconfig.com = 13;
 
 % Habituation
@@ -20,11 +16,7 @@ vrconfig.blackoutDuration = .2; % min
 
 % Trial
 vrconfig.trialDuration = 5; % min
-<<<<<<< HEAD
-vrconfig.timeoutDuration = 0; % sec
-=======
 vrconfig.timeoutDuration = .2; % sec
->>>>>>> refs/remotes/origin/master
 vrconfig.paddingDuration = .1   ; % min
 
 % Stress
