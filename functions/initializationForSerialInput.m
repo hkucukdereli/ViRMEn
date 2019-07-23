4f9e03995fbcdc41a4d86169298ca392af957d31
Hakan Kucukdereli <hkucukdereli@gmail.com>
2019-07-23 12:37:53 -0400

    Initialization code added.

---
+++ functions/initializationForSerialInput.m
@@ -0,0 +1,15 @@
+function vr = initializationForSerialInput(vr, com)
+    fprintf('Initializing serial input...\n');
+    % open or create binary file for writing and store its file ID in vr
+%     filename = sprintf('virmenLog_%s_%s_%i.dat', vr.date, vr.mouse, vr.run);
+%     vr.fid = fopen(filename, 'w');
+    
+    % --- Initialize and set up serial communication with Arduino ---%
+    vr.arduino_data = [];
+    try
+        vr.arduino_serial_input = arduinoOpen(com);
+        fprintf('Arduino is connected to COM%i.\n', com);
+    catch
+        fprintf('COM%i is not available or Arduino is not connected. Continuing without serial...\n', com);
+    end
+end
