0af32cf4a812c6357b3c0e33ac3bb295eb78e7fb
Hakan Kucukdereli <hkucukdereli@gmail.com>
2019-10-14 11:48:28 -0400

    Bug fix for parabolic screen

--- transformations/fliptransformCyl_NPparab.m
+++ transformations/fliptransformCyl_NPparab.m
@@ -15,7 +15,7 @@ function [coords2D, out_win2] = fliptransformCyl_NPparab(coords3D)
     azimuth = atan2(coords3D(2,:),coords3D(1,:));
 
     % height = tan(elevation)*5;
-    azimuth = wrapToPi(azimuth);
+    % azimuth = wrapToPi(azimuth);
     visible = ~(azimuth<-pi/4 & azimuth>(-pi + pi/4));
 
     xSign = sign(coords3D(1,:));
@@ -46,3 +46,38 @@ function [coords2D, out_win2] = fliptransformCyl_NPparab(coords3D)
     coords2D(2,coords2D(2,:)<-1) = -1;
 
     coords2D(3,:) = visible;
+
+end
+
+% function lambda = wrapToPi(lambda)
+%     %wrapToPi Wrap angle in radians to [-pi pi]
+%     %
+%     %   lambdaWrapped = wrapToPi(LAMBDA) wraps angles in LAMBDA, in radians,
+%     %   to the interval [-pi pi] such that pi maps to pi and -pi maps to
+%     %   -pi.  (In general, odd, positive multiples of pi map to pi and odd,
+%     %   negative multiples of pi map to -pi.)
+%     %
+%     %   See also wrapTo2Pi, wrapTo180, wrapTo360.
+% 
+%     % Copyright 2007-2008 The MathWorks, Inc.
+% 
+%     q = (lambda < -pi) | (pi < lambda);
+%     lambda(q) = wrapTo2Pi(lambda(q) + pi) - pi;
+% end
+% 
+% function lambda = wrapTo2Pi(lambda)
+%     %wrapTo2Pi Wrap angle in radians to [0 2*pi]
+%     %
+%     %   lambdaWrapped = wrapTo2Pi(LAMBDA) wraps angles in LAMBDA, in radians,
+%     %   to the interval [0 2*pi] such that zero maps to zero and 2*pi maps
+%     %   to 2*pi. (In general, positive multiples of 2*pi map to 2*pi and
+%     %   negative multiples of 2*pi map to zero.)
+%     %
+%     %   See also wrapToPi, wrapTo180, wrapTo360.
+% 
+%     % Copyright 2007-2008 The MathWorks, Inc.
+% 
+%     positiveInput = (lambda > 0);
+%     lambda = mod(lambda, 2*pi);
+%     lambda((lambda == 0) & positiveInput) = 2*pi;
+% end
